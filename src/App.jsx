beb20a38a0cf84da32c2d0f9107565e465482983
Vaibhav <softbyvaibhav01@gmail.com>
2025-06-21 09:27:13 +0530

    lorem picsum add Api

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <div className="image bg-gradient-to-br from-gray-100 to-white w-full py-10 px-4 md:px-10">
-        <Images />
-      </div>
+      <Images/>
     </>
   )
 }
